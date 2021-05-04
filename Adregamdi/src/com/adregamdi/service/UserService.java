17fcc68e9471e91d44ce6903fee31593fc76f72a
SJB_ <SJB_@118.33.80.249>
2021-05-05 00:45:02 +0900

    login fix

--- Adregamdi/src/com/adregamdi/service/UserService.java
+++ Adregamdi/src/com/adregamdi/service/UserService.java
@@ -1,12 +1,9 @@
 package com.adregamdi.service;
 
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.adregamdi.dao.UserDAO;
-import com.adregamdi.dto.UserDTO;
 
 @Service
 public class UserService {
@@ -14,8 +11,9 @@ public class UserService {
 	@Autowired
 	private UserDAO userDAO;
 	
-	@Resource(name="loginUserDTO")
-	private UserDTO loginUserDTO;
+	/*
+	 * @Resource(name="loginUserDTO") private UserDTO loginUserDTO;
+	 */
 	
 	public boolean checkID(String user_id) {
 		String checking_id = userDAO.checkID(user_id);
