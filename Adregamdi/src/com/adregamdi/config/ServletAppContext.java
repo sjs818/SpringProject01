541706a7d33d233b7583ee79fcf67e9599b36778
황인용 <merong1005@icloud.com>
2021-05-06 09:48:24 +0900

    servlet mapper이름 변경

--- Adregamdi/src/com/adregamdi/config/ServletAppContext.java
+++ Adregamdi/src/com/adregamdi/config/ServletAppContext.java
@@ -18,7 +18,7 @@ import com.adregamdi.mapper.Board01Mapper;
 import com.adregamdi.mapper.Board02Mapper;
 import com.adregamdi.mapper.Board03Mapper;
 import com.adregamdi.mapper.Board04Mapper;
-import com.adregamdi.mapper.Board05Mapper;
+import com.adregamdi.mapper.TogetherMapper;
 import com.adregamdi.mapper.UserMapper;
 
 @Configuration
@@ -98,8 +98,8 @@ public class ServletAppContext implements WebMvcConfigurer{
 	}
 	
 	@Bean
-	public MapperFactoryBean<Board05Mapper> getBoard05Mapper(SqlSessionFactory factory) {
-		MapperFactoryBean<Board05Mapper> factoryBean = new MapperFactoryBean<Board05Mapper>(Board05Mapper.class);
+	public MapperFactoryBean<TogetherMapper> getTogetherMapper(SqlSessionFactory factory) {
+		MapperFactoryBean<TogetherMapper> factoryBean = new MapperFactoryBean<TogetherMapper>(TogetherMapper.class);
 		factoryBean.setSqlSessionFactory(factory);
 		return factoryBean;
 	}
