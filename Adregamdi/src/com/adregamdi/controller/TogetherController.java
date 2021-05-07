7dc3690835d0c6c477243208d5d7082c1f879e4a
황인용 <merong1005@icloud.com>
2021-05-07 18:04:24 +0900

    project day5

--- Adregamdi/src/com/adregamdi/controller/TogetherController.java
+++ Adregamdi/src/com/adregamdi/controller/TogetherController.java
@@ -1,24 +1,41 @@
 package com.adregamdi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.adregamdi.dto.PageDTO;
 import com.adregamdi.dto.TogetherDTO;
+import com.adregamdi.service.TogetherService;
 
 @Controller
 @RequestMapping("/together")
 public class TogetherController {
 	
-
-	@GetMapping("/list")
-	public String list() {
+	@Autowired
+	private TogetherService togetherService;
+	
+	/*@GetMapping("/list")
+	public String list(@RequestParam("page") int page, Model model) {
+		
+		List<TogetherDTO> togetherList = togetherService.getTogetherList(page, 4);
+		model.addAttribute("togetherList", togetherList);
+		
+		PageDTO pageDTO = togetherService.getContentCnt(page, 4, 10);
+		model.addAttribute("pageDTO", pageDTO);
+		
 		return "together/list";
 	}
-	
+	*/
 	@GetMapping("/write")
-	public String write(@ModelAttribute("writeTogetherDTO")TogetherDTO writeTogetherDTO) {
+	public String write(@ModelAttribute("writeTogetherDTO")TogetherDTO writeTogetherDTO,
+			@RequestParam("page") int page, Model model) {
 		return "together/write";
 	}
 
