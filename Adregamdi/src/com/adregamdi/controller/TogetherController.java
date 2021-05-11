fd5b6c25185bee4d36493e5e1ba5afd83a0b58e3
황인용 <merong1005@icloud.com>
2021-05-11 09:41:56 +0900

    컨트롤러 DAO MAPPER삽입

--- Adregamdi/src/com/adregamdi/controller/TogetherController.java
+++ Adregamdi/src/com/adregamdi/controller/TogetherController.java
@@ -1,58 +1,89 @@
 package com.adregamdi.controller;
 
 import java.util.List;
-
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.adregamdi.dto.PageDTO;
 import com.adregamdi.dto.TogetherDTO;
 import com.adregamdi.service.TogetherService;
 
 @Controller
 @RequestMapping("/together")
 public class TogetherController {
-	
+
 	@Autowired
-	private TogetherService togetherService;
+	TogetherService togetherService;
 	
-	/*@GetMapping("/list")
-	public String list(@RequestParam("page") int page, Model model) {
-		
-		List<TogetherDTO> togetherList = togetherService.getTogetherList(page, 4);
-		model.addAttribute("togetherList", togetherList);
-		
-		PageDTO pageDTO = togetherService.getContentCnt(page, 4, 10);
-		model.addAttribute("pageDTO", pageDTO);
-		
+	@GetMapping("/list")
+	public String BoardList(Model model) {
+		List<TogetherDTO> contentList = TogetherService.getToghetherList();
+		model.addAttribute("contentList", contentList);
 		return "together/list";
 	}
-	*/
-	@GetMapping("/write")
-	public String write(@ModelAttribute("writeTogetherDTO")TogetherDTO writeTogetherDTO,
-			@RequestParam("page") int page, Model model) {
-		return "together/write";
-	}
-
+	
 	@GetMapping("/delete")
-	public String delete() {
+	public String BoardDelete() {
 		return "together/delete";
 	}
 	
+	@GetMapping("/read")
+	public String BoardRead(@RequestParam("content_idx") int content_idx, Model model) {
+		TogetherDTO readContentDTO = TogetherService.getTogetherContent(content_idx);
+		model.addAttribute("readContentDTO", readContentDTO);
+		return "together/read";
+	}
+	
+	@GetMapping("/write")
+	public String BoardWrite
+	(@ModelAttribute("TogetherWriteDTO") TogetherDTO TogetherDTO) {
+		return "together/write";
+	}
+	
+	@PostMapping("/writeProc")
+	public String Together_Proc
+	(@Valid @ModelAttribute("TogetherDTO") TogetherDTO TogetherDTO, BindingResult result) {
+		if(result.hasErrors())
+			return "together/write";
+		
+		TogetherService.InsertTogetherContent(togetherWriteDTO);
+		
+		return "together/write_success";
+	}
+	
 	@GetMapping("/modify")
-	public String modify() {
+	public String BoardModify
+	(@ModelAttribute("TogetherDTO") TogetherDTO togetherModifyDTO, 
+	 @RequestParam("content_idx") int content_idx, Model model) {
+		
+		TogetherDTO TogetherDTO = togetherService.getTogetherContent(content_idx);
+		
+		togetherModifyDTO.setto_no(TogetherDTO.getto_no());
+		togetherModifyDTO.setto_writer(TogetherDTO.getto_writher());
+		togetherModifyDTO.setto_title(TogetherDTO.getto_title());
+		togetherModifyDTO.setto_cnt(TogetherDTO.getto_cnt());
+		togetherModifyDTO.setto_date(TogetherDTO.getto_date());
+		togetherModifyDTO.setto_content(TogetherDTO.getto_content());
+		
 		return "together/modify";
 	}
 	
-	@GetMapping("/read")
-	public String read() {
-		return "together/read";
-	}		
+	@PostMapping("/modifyProc")
+	public String BoardModify_Proc
+	(@Valid @ModelAttribute("togetherModifyDTO") TogetherDTO togetherModifyDTO, BindingResult result) {
+		
+		togetherService.ModifytogetherContent(togetherModifyDTO);
+		
+		return "together/modify_success";
+	}
+	
 }
 	
 	
--- Adregamdi/src/com/adregamdi/mapper/TogetherMapper.java
+++ Adregamdi/src/com/adregamdi/mapper/TogetherMapper.java
@@ -1,5 +1,34 @@
 package com.adregamdi.mapper;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import com.adregamdi.dto.TogetherDTO;
+
 public interface TogetherMapper {
+	
+	@Insert("INSERT INTO COMMUNITY_TO(TO_NO, TO_WRITHER, TO_TITLE, TO_CNT, TO_DATE, TO_CONTENT)" +
+			"VALUES(CONTENT_CNT_SEQ*nextval, 1, #{to_title}, 0, SYSDATE, #{to_content})" )
+	void InsertTogetherContent(TogetherDTO togetherDTO);
+	
+	@Select(" SELECT T.TO_NO, U.USER_ID CONTENT_WRITER_ID, T.TO_TITLE, T.TO_CNT, "
+				+ " 			 TO_CHAR(T.TO_DATE, 'YYYY-MM-DD HH24:MI:SS') CONTENT_DATE " 
+			  + " 	FROM COMMUNITY_TO C, USER_INFO U " 
+				+ " WHERE T.TO_WRITER =  U.USER_NO "
+			  + " ORDER BY T.TO_NO DESC")
+	List<TogetherDTO> getTogetherList();
+	
+	@Select("SELECT T.TO_NO, U.USER_ID CONTENT_WRITER_ID, "
+				+ "TO_CHAR(T.TO_DATE, 'YYYY-MM-DD HH24:MI:SS') CONTENT_DATE, "
+			  + "T.TO_TITLE, T.TO_CONTENT, T.TO_CNT "
+				+ "FROM USER_INFO U, COMMUNITY_TO  "
+			  + "WHERE T.TO_WRITER = U.USER_NO "
+				+ "AND T.TO_NO = #{content_idx}")
+	TogetherDTO getTogetherContent(int content_idx);
+	
+	@update("UPDATE COMMUNITY_TO SET TO_TITLE=#{to_title}, "
+			  +"TO_CONTENT=#{to_content}, to_date=SYSDATE "
+			  +"WHERE TO_NO = #{to_no}")
+	void modifyTogetherContent(TogetherDTO togetherDTO);
 
 }
