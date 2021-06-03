df2028e644602c44357c03a0a3c089e415b82986
JB <sunjongbin11@naver.com>
2021-06-03 11:49:29 +0900

    TOGETHER DTO 수정

--- Adregamdi/src/com/adregamdi/dto/TogetherDTO.java
+++ Adregamdi/src/com/adregamdi/dto/TogetherDTO.java
@@ -6,24 +6,29 @@ import lombok.Setter;
 
 @Setter @Data @Getter
 public class TogetherDTO {
-	
-	// 글번호
-	private int to_no;
-	// 작성자
-	private int to_writer;
-	// 글 제목
-	private String to_title;
-	private String to_place;
-	// 조회수
-	private int to_curr;
-	// 작성날짜
-	private String to_date;
-	// 글 내용
-	private String to_content;
-	// 유저 비밀번호
-	private int to_total;
-	// 유저 ID
-	private String to_meet;
-	private int to_state;
-	private int status;
+	   
+	   // 게시글번호
+	   private int to_no;
+	   // 회원번호
+	   private int to_writer_no;
+	   // 회원번호
+	   private String to_writer;
+	   // 공고제목
+	   private String to_title;
+	   // 여행장소
+	   private String to_place;
+	  //여행지
+	   private String to_place_name;
+	   // 공고문
+	   private String to_content;
+	   // 작성날짜
+	   private String to_date;
+	   // 현재인원
+	   private int to_curr;
+	   // 모집인원
+	   private int to_total;
+	   // 여행날짜
+	   private String to_meet;
+	   // 공고현황
+	   private int to_state;   
 }
