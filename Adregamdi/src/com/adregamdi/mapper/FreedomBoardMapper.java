d1ac55b7536c79c07f073eff1033ee5dfed0ecff
Seonbox <60653941+Ramsr-git@users.noreply.github.com>
2021-08-20 17:21:30 +0900

    Revert "Hs"

--- Adregamdi/src/com/adregamdi/mapper/FreedomBoardMapper.java
+++ Adregamdi/src/com/adregamdi/mapper/FreedomBoardMapper.java
@@ -34,7 +34,7 @@ public interface FreedomBoardMapper {
 			"TO_CHAR(F.FREE_DATE, 'YYYY-MM-DD HH24:MI:SS') CONTENT_DATE, " + 
 			"(SELECT COUNT(*) FROM FREEDOMREPLY R WHERE F.FREE_NO = R.FREEDOM_NUM) REPLY_COUNT " + 
 			"FROM FREEDOMBOARD F, USER_INFO U " + 
-			"WHERE F.FREE_WRITER = U.USER_NO AND (F.FREE_TITLE || F.FREE_CONTENT) LIKE '%' || #{keywords} || '%' " + 
+			"WHERE F.FREE_WRITER = U.USER_NO AND F.FREE_TITLE LIKE '%' || #{keywords} || '%' " + 
 			"AND F.FREE_CONTENT LIKE '%' || #{keywords} || '%' ")
 	List<FreedomBoardDTO> getSearchKeyObejctContentFreedomBoardList(RowBounds rowBounds, String keywords);
 	
@@ -75,7 +75,8 @@ public interface FreedomBoardMapper {
 	//게시글 제목 + 내용 검색한 카운트 불러와 페이징 처리
 	@Select("SELECT COUNT(*) " + 
 			"FROM FREEDOMBOARD F, USER_INFO U " + 
-			"WHERE F.FREE_WRITER = U.USER_NO AND (F.FREE_TITLE || F.FREE_CONTENT) LIKE '%' || #{keywords} ||'%' ")
+			"WHERE F.FREE_WRITER = U.USER_NO AND F.FREE_TITLE LIKE '%' || #{keywords} ||'%' " + 
+			"OR F.FREE_CONTENT LIKE '%' || #{keywords} || '%' ")
 	int getSearchKeyObjectContent(String keywords);
 	
 	//게시글 아이디로 검색해서 불러온 카운트
